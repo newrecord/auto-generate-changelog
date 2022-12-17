858c01de0d65f94e8b0fada0d96d48f7a1a6a7da
newrecord <newrecord@naver.com>
2022-12-17 15:28:41 +0900

    add: project

---
+++ app/src/main/java/com/newrecord/changelog/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.newrecord.changelog.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
